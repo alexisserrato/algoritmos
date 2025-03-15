8ee10ec734e3bcf8396000a9fe507866caf9beec
alexisserrato <alexisserrato1992@gmail.com>
2025-03-14 19:24:27 -0500

    introduccion a python

---
+++ uso_de_python.py
@@ -0,0 +1,27 @@
+#Autor:Alexis Serrato
+#este es un comentario de una sola linea
+''''
+este es un comentario de  '
+multiples'
+lineas
+
+'''
+print("Estudiando python")
+
+#definicion de variables
+'''
+
+reglas:
+python es sensible a minusculas y mayúsculas
+no se pueden usar espacio
+no se pueden usar palabras reservadas
+no es conveniente usar numeros como nombres de variables
+
+'''
+x=50
+y=15
+
+print(x+y)
+entrada_de_datos=
+print(type(x))
+#class 
